4b92aac92ca26718bd2453eda5dddf3136ee1100
Wendy Nie <wendy90n@gmail.com>
2022-11-20 23:16:46 -0800

    incorporating facemesh

--- page_turner.js
+++ page_turner.js
@@ -9,17 +9,27 @@ var pdfjsLib = window['pdfjs-dist/build/pdf'];
 // The workerSrc property shall be specified.
 pdfjsLib.GlobalWorkerOptions.workerSrc = 'pdf.worker.js';
 
+/**
+ *PDF variables
+ */ 
 var pdfDoc = null,
     pageNum = kPageOne,
     pageRendering = false,
     pageNumPending = null,
     scale = 0.8,
-    canvas = null,
-    ctx = null;
+    pdfCanvas = null,
+    pdfCtx = null;
+
+/**
+ *Camera variables
+ */
+var video = null,
+    videoCanvas = null,
+    videoCtx = null;
 
 function initOnLoad() {
-    canvas = document.getElementById('pdf-canvas');
-    ctx = canvas.getContext('2d');
+    pdfCanvas = document.getElementById('pdf-canvas');
+    pdfCtx = pdfCanvas.getContext('2d');
     document.getElementById('prev').addEventListener('click', onPrevPage);
     document.getElementById('next').addEventListener('click', onNextPage);
 
@@ -34,6 +44,19 @@ function initOnLoad() {
       }
       fileReader.readAsArrayBuffer(file);
     });
+
+    video = document.getElementsByClassName('video')[0];
+    videoCanvas = document.getElementsByClassName('video-canvas')[0];
+    videoCtx = videoCanvas.getContext('2d');
+
+    const camera = new Camera(video, {
+      onFrame: async () => {
+        await faceMesh.send({image: video});
+      },
+      width: 1280,
+      height: 720
+    });
+    camera.start();
 }
 
 window.onload = initOnLoad
@@ -47,12 +70,12 @@ function renderPage(num) {
   // Using promise to fetch the page
   pdfDoc.getPage(num).then(function(page) {
     var viewport = page.getViewport({scale: scale});
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
+    pdfCanvas.height = viewport.height;
+    pdfCanvas.width = viewport.width;
 
     // Render PDF page into canvas context
     var renderContext = {
-      canvasContext: ctx,
+      canvasContext: pdfCtx,
       viewport: viewport
     };
     var renderTask = page.render(renderContext);
@@ -120,3 +143,36 @@ function updatePdfDoc(newPdfDoc) {
 
 
 pdfjsLib.getDocument(kDefaultPdf).promise.then(updatePdfDoc);
+
+function onResults(results) {
+  videoCtx.save();
+  videoCtx.clearRect(0, 0, videoCanvas.width, videoCanvas.height);
+  videoCtx.drawImage(
+      results.image, 0, 0, videoCanvas.width, videoCanvas.height);
+  if (results.multiFaceLandmarks) {
+    for (const landmarks of results.multiFaceLandmarks) {
+      drawConnectors(videoCtx, landmarks, FACEMESH_TESSELATION,
+                     {color: '#C0C0C070', lineWidth: 1});
+      drawConnectors(videoCtx, landmarks, FACEMESH_RIGHT_EYE, {color: '#FF3030'});
+      drawConnectors(videoCtx, landmarks, FACEMESH_RIGHT_EYEBROW, {color: '#FF3030'});
+      drawConnectors(videoCtx, landmarks, FACEMESH_RIGHT_IRIS, {color: '#FF3030'});
+      drawConnectors(videoCtx, landmarks, FACEMESH_LEFT_EYE, {color: '#30FF30'});
+      drawConnectors(videoCtx, landmarks, FACEMESH_LEFT_EYEBROW, {color: '#30FF30'});
+      drawConnectors(videoCtx, landmarks, FACEMESH_LEFT_IRIS, {color: '#30FF30'});
+      drawConnectors(videoCtx, landmarks, FACEMESH_FACE_OVAL, {color: '#E0E0E0'});
+      drawConnectors(videoCtx, landmarks, FACEMESH_LIPS, {color: '#E0E0E0'});
+    }
+  }
+  videoCtx.restore();
+}
+
+const faceMesh = new FaceMesh({locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+}});
+faceMesh.setOptions({
+  maxNumFaces: 1,
+  refineLandmarks: true,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
+});
+faceMesh.onResults(onResults);
